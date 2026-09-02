82ea4b81863f2cc2229c99634e462a257f99c6a7
ds231002 <ds231002@ustp-students.at>
2026-09-02 04:53:34 +0200

    update

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -1,14 +1,14 @@
 from utils.files import load_json
 import pandas as pd
 
-# def load_result_json(
-#     task_id: str,
-#     task_type: str,
-#     method: str,
-#     model: str
-# ):
-#     path = f"output/results/{task_type}/{task_id}/{method}_{model.replace(':', '_')}.json"
-#     return load_json(path)
+def load_result_json(
+    task_id: str,
+    task_type: str,
+    method: str,
+    model: str
+):
+    path = f"output/results/{task_type}/{task_id}/{method}_{model.replace(':', '_')}.json"
+    return load_json(path)
 
 def adapt_evaluation_df(df: pd.DataFrame) -> pd.DataFrame:
     # fill NaN from combined cells
